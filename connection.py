13face8fc8ec0e681968a67ac74a8d9b8847f2f6
David Castagneto Aguirre <davidcastagnetoa@gmail.com>
2023-10-24 15:01:40 +0200

    user.db

--- connection.py
+++ connection.py
@@ -1,7 +1,13 @@
 import sqlite3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ADMIN_PASS = os.getenv("ADMIN_PASS")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 
 
-class ConnectionDB():
+class ConnectionDB:
     def __init__(self):
         try:
             self.con = sqlite3.connect("users.db")
@@ -10,7 +16,34 @@ class ConnectionDB():
             print(error)
 
     def createTable(self):
-        # cur = self.con.cursor()
-        # cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)")
-        # self.con.commit()
-        sql_create_table = """CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)"""
+        sql_create_table = """
+        CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        email TEXT,
+        password TEXT
+        )
+        """
+        cur = self.con.cursor()
+        cur.execute(sql_create_table)
+        cur.close()
+        self.createAdmin()
+
+    def createAdmin(self):
+        try:
+            sql_insert = """
+            INSERT INTO users values(null, '{}', '{}', '{}')
+            """.format(
+                ADMIN_USERNAME, "davidcastagnetoa@gmail.com", ADMIN_PASS
+            )  # Remeber to hide the password in .env File
+            cur = self.con.cursor()
+            cur.execute(sql_insert)
+            self.con.commit()
+        except Exception as error:
+            print(f"El usuario {error} ya existe")
+
+    def connect():
+        print()
+
+
+con = ConnectionDB()
